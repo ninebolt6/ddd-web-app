8c7dc5b553428112f49ec4d8e617b3bfa5e8761d
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-14 20:56:26 +0900

    fix: テストが書けないのでmacroを使うのをやめた

--- app/src/example/presentation/example.rs
+++ app/src/example/presentation/example.rs
@@ -1,11 +1,10 @@
-use actix_web::{dev::HttpServiceFactory, get, services, HttpResponse};
+use actix_web::{web, HttpResponse};
 use share::common::result::ResponseResult;
 
-pub fn example_routes() -> impl HttpServiceFactory {
-    services![hello]
+pub fn example_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/hello").route(web::get().to(hello)));
 }
 
-#[get("/hello")]
 async fn hello() -> ResponseResult {
     Ok(HttpResponse::Ok().body("Hello world!"))
 }
--- app/src/route.rs
+++ app/src/route.rs
@@ -4,7 +4,7 @@ use crate::example::presentation::example::example_routes;
 
 pub fn public_routes(cfg: &mut web::ServiceConfig) {
     #[cfg(debug_assertions)]
-    cfg.service(example_routes());
+    example_routes(cfg);
 }
 
 pub fn auth_routes(cfg: &mut web::ServiceConfig) {}

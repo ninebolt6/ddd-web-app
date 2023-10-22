a2dcb38708213e66eb8ec170555d9284c17b53c7
ninebolt6 <67471674+ninebolt6@users.noreply.github.com>
2023-10-23 00:44:24 +0900

    feat: example用のUserRepositoryを作成

---
+++ shared/src/example/infrastructure/database/repository/user_repository.rs
@@ -0,0 +1,31 @@
+use async_trait::async_trait;
+use sqlx::{FromRow, PgPool};
+
+use crate::{common::error::APIError, example::domain::entity::user::UserEntity};
+
+#[async_trait]
+pub trait UserRepository {
+    async fn find_by_id(&self, id: i32, pool: &PgPool) -> Result<UserEntity, APIError>;
+}
+
+pub struct UserRepositoryImpl {}
+
+#[async_trait]
+impl UserRepository for UserRepositoryImpl {
+    async fn find_by_id(&self, id: i32, pool: &PgPool) -> Result<UserEntity, APIError> {
+        #[derive(FromRow)]
+        struct UserRow {
+            id: i32,
+            name: String,
+        }
+
+        let user_row = sqlx::query_as::<_, UserRow>("SELECT id, name FROM users WHERE id = $1")
+            .bind(id)
+            .fetch_one(pool)
+            .await
+            .map_err(|e| APIError::InfrastructureError(e.to_string()))?;
+
+        let user = UserEntity::new(user_row.id, user_row.name);
+        Ok(user)
+    }
+}

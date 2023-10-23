ef4117d51d9e591ee5a079fb7baf45cc2719ebf2
ninebolt6 <67471674+ninebolt6@users.noreply.github.com>
2023-10-23 10:59:18 +0900

    feat: injectorをグローバルに扱えるようにする

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -3,17 +3,23 @@ pub mod route;
 #[cfg(debug_assertions)]
 pub mod example;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{
+    web::{self, Data},
+    App, HttpServer,
+};
+use shared::{common::injector::Injector, external::database::ConnectionFactoryImpl};
 
 pub async fn run_server() -> Result<(), std::io::Error> {
     HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api").configure(route::public_routes).service(
-                web::scope("")
-                    // .wrap(auth_middleware)
-                    .configure(route::auth_routes),
-            ),
-        )
+        App::new()
+            .service(
+                web::scope("/api").configure(route::public_routes).service(
+                    web::scope("")
+                        // .wrap(auth_middleware)
+                        .configure(route::auth_routes),
+                ),
+            )
+            .app_data(Data::new(Injector::new(ConnectionFactoryImpl)))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- shared/src/common/injector.rs
+++ shared/src/common/injector.rs
@@ -2,19 +2,11 @@ use crate::external::database::ConnectionFactory;
 
 pub struct Injector<CF>
 where
-    Self: Clone,
     CF: ConnectionFactory,
 {
     connection_factory: CF,
 }
 
-impl<CF: ConnectionFactory + Clone> Clone for Injector<CF> {
-    fn clone(&self) -> Self {
-        Self {
-            connection_factory: self.connection_factory.clone(),
-        }
-    }
-}
 impl<CF: ConnectionFactory + Clone> Injector<CF> {
     pub fn new(connection_factory: CF) -> Self {
         Self { connection_factory }

70bb61ff9aafe378943a809777eeac225a525a5b
ninebolt6 <67471674+ninebolt6@users.noreply.github.com>
2023-10-23 13:07:32 +0900

    feat: ユーザを作成・取得するエンドポイントを追加

--- app/src/example/presentation/example.rs
+++ app/src/example/presentation/example.rs
@@ -1,61 +1,106 @@
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize, Serialize};
-use shared::common::result::ResponseResult;
+use shared::{
+    common::{injector::Injector, result::ResponseResult},
+    external::database::{ConnectionFactory, ConnectionFactoryImpl},
+};
+use uuid::Uuid;
+
+use crate::example::usecase::interactor::{
+    create_user::CreateUserInteractor, get_user::GetUserInteractor,
+};
 
 pub fn example_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/hello").route(web::get().to(hello)))
-        .service(web::resource("/hello/{text}").route(web::post().to(post_hello)));
+    cfg.service(
+        web::resource("/users").route(web::post().to(create_user::<ConnectionFactoryImpl>)),
+    )
+    .service(web::resource("/users/{id}").route(web::get().to(get_user::<ConnectionFactoryImpl>)));
 }
 
-async fn hello() -> ResponseResult {
-    Ok(HttpResponse::Ok().body("Hello world!"))
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+struct GetUserResponse {
+    id: Uuid,
+    name: String,
+}
+
+async fn get_user<CF: ConnectionFactory>(
+    path: web::Path<Uuid>,
+    injector: web::Data<Injector<CF>>,
+) -> ResponseResult {
+    let id = path.into_inner();
+
+    let output = GetUserInteractor::execute(id, injector.connection_factory()).await?;
+
+    Ok(HttpResponse::Ok().json(GetUserResponse {
+        id: output.id,
+        name: output.name,
+    }))
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
-struct PostHelloRequest {
+struct CreateUserRequest {
     user_name: String,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
-#[serde(rename_all = "camelCase")]
-struct PostHelloResponse {
-    message: String,
-    text: String,
-}
-
-async fn post_hello(path: web::Path<String>, body: web::Json<PostHelloRequest>) -> ResponseResult {
-    let path_param = path.into_inner();
+async fn create_user<CF: ConnectionFactory>(
+    body: web::Json<CreateUserRequest>,
+    injector: web::Data<Injector<CF>>,
+) -> ResponseResult {
     let user_name = &body.user_name;
 
-    Ok(HttpResponse::Ok().json(PostHelloResponse {
-        message: format!("Hello, {user_name}!"),
-        text: path_param.repeat(2),
-    }))
+    CreateUserInteractor::execute(user_name, injector.connection_factory()).await?;
+
+    Ok(HttpResponse::Created().finish())
 }
 
 #[cfg(test)]
 mod tests {
-    use actix_web::{body::to_bytes, http::StatusCode, web};
+    use std::str::FromStr;
+
+    use actix_web::{
+        body::to_bytes,
+        http::StatusCode,
+        web::{self, Data},
+    };
 
     use super::*;
 
     #[actix_web::test]
-    async fn test_post_hello() {
-        let path = web::Path::from("test".to_string());
-        let body = web::Json(PostHelloRequest {
-            user_name: "test name".to_string(),
-        });
+    async fn test_get_user() {
+        let id = Uuid::from_str("ca1c5eb2-a43a-4cef-80fc-f9abe1623785").unwrap();
+        let path = web::Path::from(id);
 
-        let resp = post_hello(path, body).await.unwrap();
+        let resp = get_user::<ConnectionFactoryImpl>(
+            path,
+            Data::new(Injector::new(ConnectionFactoryImpl)),
+        )
+        .await
+        .unwrap();
+
         assert_eq!(resp.status(), StatusCode::OK);
         let resp_body = to_bytes(resp.into_body()).await.unwrap();
         assert_eq!(
-            serde_json::from_slice::<PostHelloResponse>(&resp_body).unwrap(),
-            PostHelloResponse {
-                message: "Hello, test name!".to_string(),
-                text: "testtest".to_string(),
+            serde_json::from_slice::<GetUserResponse>(&resp_body).unwrap(),
+            GetUserResponse {
+                id: id,
+                name: "test name".to_string(),
             }
         );
+    }
+
+    #[actix_web::test]
+    async fn test_create_user() {
+        let body = web::Json(CreateUserRequest {
+            user_name: "test name".to_string(),
+        });
+
+        let resp = create_user::<ConnectionFactoryImpl>(
+            body,
+            Data::new(Injector::new(ConnectionFactoryImpl)),
+        )
+        .await
+        .unwrap();
+        assert_eq!(resp.status(), StatusCode::CREATED);
     }
 }
